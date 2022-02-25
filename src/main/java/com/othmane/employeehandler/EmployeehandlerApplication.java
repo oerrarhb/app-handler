3755eb5d628bbc93cabd2e86d6db35788dbe3d6b
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2022-02-25 17:57:39 +0100

    add commandlinerunner

--- src/main/java/com/othmane/employeehandler/EmployeehandlerApplication.java
+++ src/main/java/com/othmane/employeehandler/EmployeehandlerApplication.java
@@ -1,12 +1,28 @@
 package com.othmane.employeehandler;
 
+import com.othmane.employeehandler.model.Employee;
+import com.othmane.employeehandler.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeehandlerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeehandlerApplication.class, args);
-	}
+public class EmployeehandlerApplication implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeehandlerApplication.class, args);
+    }
 
+
+    private final EmployeeRepository repository;
+
+    @Autowired
+    public EmployeehandlerApplication(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+    }
 }
