70cf71be196d4873665108438c21e7dca78de9f5
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2022-02-26 08:52:01 +0100

    add get employees

--- src/main/java/com/othmane/employeehandler/EmployeehandlerApplication.java
+++ src/main/java/com/othmane/employeehandler/EmployeehandlerApplication.java
@@ -24,5 +24,8 @@ public class EmployeehandlerApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+        this.repository.save(new Employee("Daryl", "Dixon", "Archer"));
+        this.repository.save(new Employee("Jhon", "Whick", "Agent"));
+
     }
 }
--- src/main/java/com/othmane/employeehandler/controller/HomeController.java
+++ src/main/java/com/othmane/employeehandler/controller/HomeController.java
@@ -1,14 +1,26 @@
 package com.othmane.employeehandler.controller;
 
-import org.springframework.stereotype.Controller;
+import com.othmane.employeehandler.model.Employee;
+import com.othmane.employeehandler.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/employees")
 public class HomeController {
 
-    @RequestMapping(value = "/")
-    public String index() {
-        return "index";
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    private List<Employee> getEmployee() {
+        return (List<Employee>) this.employeeRepository.findAll();
     }
 
 }
